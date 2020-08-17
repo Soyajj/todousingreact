30afebb88bbb0f90e156fe8a7ccad5ea2eb7e1f4
Soyaj Bajracharya <coolboysoyaz@gmail.com>
2020-08-17 17:40:11 +0545

    Updates

---
+++ precache-manifest.838d85e3414f456be96b138ac6d5bde8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21aba9853886d5d9eee06539fa327dd9",
+    "url": "/todousingreact/index.html"
+  },
+  {
+    "revision": "2b6225a7c8e7d8f37bed",
+    "url": "/todousingreact/static/css/main.6a08e227.chunk.css"
+  },
+  {
+    "revision": "76d7981437226385d2a0",
+    "url": "/todousingreact/static/js/2.804e7d91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todousingreact/static/js/2.804e7d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b6225a7c8e7d8f37bed",
+    "url": "/todousingreact/static/js/main.ec31075d.chunk.js"
+  },
+  {
+    "revision": "73f33336f221ae6efedf",
+    "url": "/todousingreact/static/js/runtime-main.b732015a.js"
+  }
+]);
